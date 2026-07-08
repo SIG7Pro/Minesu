a4e06dd82fe9b663143875934c6a4b466397b34b
ElastedAlorian <82591291+ElastedAlorian@users.noreply.github.com>
2026-07-08 16:54:03 -0400

    Revisiting + Some Old Code
    Some old code is here too.
    
    The code adding stuff to Discord RPC stuff is new, the rest is old, and there's a bit more when it comes to the layouting and stuff.

--- source/Main.hx
+++ source/Main.hx
@@ -4,6 +4,12 @@ import flixel.FlxGame;
 import openfl.display.Sprite;
 import flixel.FlxG;
 
+#if hxdiscord_rpc
+import hxdiscord_rpc.Discord;
+import hxdiscord_rpc.Types;
+import sys.thread.Thread;
+#end
+
 class Main extends Sprite
 {
 	public function new()
@@ -11,5 +17,85 @@ class Main extends Sprite
 		super();
 		addChild(new FlxGame(1920, 1080, Menu));
 		FlxG.mouse.load('assets/images/cursor.png');
+
+
+		#if hxdiscord_rpc
+
+		Sys.println('Initializing Discord RPC...');
+
+		final handlers:DiscordEventHandlers = new DiscordEventHandlers();
+		handlers.ready = cpp.Function.fromStaticFunction(onReady);
+		handlers.disconnected = cpp.Function.fromStaticFunction(onDisconnected);
+		handlers.errored = cpp.Function.fromStaticFunction(onError);
+		Discord.Initialize("1524305342287187979", cpp.RawPointer.addressOf(handlers), false, null);
+
+		Thread.create(function():Void
+		{
+			while (true)
+			{
+				#if DISCORD_DISABLE_IO_THREAD
+				Discord.UpdateConnection();
+				#end
+
+				Discord.RunCallbacks();
+
+				Sys.sleep(2);
+			}
+		});
+
+		/*Sys.sleep(10);
+
+		Sys.println('Shutting down Discord RPC...');
+
+		Discord.Shutdown();*/
+
+		#end
+
 	}
+
+	#if hxdiscord_rpc
+
+	private static function onReady(request:cpp.RawConstPointer<DiscordUser>):Void
+	{
+		final username:String = request[0].username;
+		final globalName:String = request[0].username;
+		final discriminator:Int = Std.parseInt(request[0].discriminator);
+
+		if (discriminator != 0)
+			Sys.println('Discord: Connected to user ${username}#${discriminator} ($globalName)');
+		else
+			Sys.println('Discord: Connected to user @${username} ($globalName)');
+
+		final discordPresence:DiscordRichPresence = new DiscordRichPresence();
+		discordPresence.type = DiscordActivityType_Watching;
+		discordPresence.state = "Prototype";
+		discordPresence.details = "Minesu";
+		discordPresence.largeImageKey = "canary-large";
+		discordPresence.smallImageKey = "ptb-small";
+
+		final button:DiscordButton = new DiscordButton();
+		button.label = "Test 1 (iiSU Website)";
+		button.url = "https://iisu.network";
+		discordPresence.buttons[0] = button;
+
+		final button:DiscordButton = new DiscordButton();
+		button.label = "Test 2 (Source Code)";
+		button.url = "https://github.com/MinesuHX/Minesu";
+		discordPresence.buttons[1] = button;
+
+		Discord.UpdatePresence(cpp.RawConstPointer.addressOf(discordPresence));
+	}
+
+	private static function onDisconnected(errorCode:Int, message:cpp.ConstCharStar):Void
+	{
+		Sys.println('Discord: Disconnected ($errorCode:$message)');
+	}
+
+	private static function onError(errorCode:Int, message:cpp.ConstCharStar):Void
+	{
+		Sys.println('Discord: Error ($errorCode:$message)');
+	}
+
+	#end
+
 }
--- source/smallMenus/HomeMesu.hx
+++ source/smallMenus/HomeMesu.hx
@@ -12,12 +12,16 @@ class HomeMesu extends FlxSubState
 
 var horizPositions:Array<Int> = [141, 341, 541, 741, 941, 1141];
 var verticPositions:Array<Int> = [302, 502, 702, 902];
+var columns:Int = 6;
+var rows:Int = 4;
 
 var iconSlot:FlxSprite;
 var curMadeSelectionIcons:FlxTypedGroup<MenuIcon>; // These both have to be changed, whoops!
 
 var homeLayout = haxe.Json.parse(sys.io.File.getContent('external/games/home_layout.json'));
 var menuArrange:Array<String>;
+var layout_xPosition_Before_Recount:Int = 0; // if position needed in the layout is larger than the amount of columns then it should wrap back to 0 by adding the column amount fo this variable and subtracting the position of an entry and this to get a position in the horizPositions' var and verticPositions too somehow
+var layout_yPosition_Before_Recount:Int = 0; // I think I'll use this simialry or in conjunction with the other, but I don't know.
 
 	public function new()
 	{
@@ -27,51 +31,64 @@ var menuArrange:Array<String>;
 	override function create()
 	{
 		super.create();
+
+		//menuArrange:new(string);
 		menuArrange = homeLayout.arrangement;
+		rows = horizPositions.length;
+		columns = verticPositions.length;
+
 
 		var deg:FlxText;
-				deg = new FlxText((122), (156)); // x, y, width
+				deg = new FlxText((FlxG.width - 600), 156, 500); // x, y, width
 				deg.text = "Layout:" + haxe.Json.stringify(menuArrange); // Very convinient...
 				//deg.text = "Extra Placeholder";
 				trace("Arrangement: " + haxe.Json.stringify(menuArrange));
 				//trace("Note: " + homeLayout.note);
-				deg.setFormat("FOT-RodinBokutohPro-B.otf", 52, 0xff403a46);
+				deg.setFormat("FOT-RodinBokutohPro-B.otf", 25, 0xff403a46);
 				deg.antialiasing = true;
 				deg.updateHitbox();
 				add(deg);
 
 		curMadeSelectionIcons = new FlxTypedGroup<MenuIcon>();
 
-		var newYPos:Int = 0;
-
 		// user slameron suggested the idea of reading from an array and then moving based on division and stuff
 					// so in theory i could turn the home_layout.json file into maybe some kinda global storage/options file too
 					// as quoted from him or her:
 						// if its all an array you could do insert on the array at the index you want it at and for the grid the x index would be the array position % columns and the y index would be Math.floor(array position / columns), i think
 
+			makeBlankSpaces();
 
-		for (h in 0...verticPositions.length){
-			for (i in 0...horizPositions.length){
-				iconSlot = new FlxSprite();
-				iconSlot.loadGraphic("assets/images/menuBody/mainMenu/Blank Icon.png");
-				iconSlot.x = horizPositions[i];
-				iconSlot.y = verticPositions[h];
-				iconSlot.updateHitbox();
-				add(iconSlot);
+		//
 
-				var testPoop:MenuIcon;
 
-				if (menuArrange.indexOf(menuArrange[i], 0) > horizPositions.length){
-					newYPos += 1; //maybe work into a new array<string?
-					// thinking of something like if (index of entry) /  columns and floored could be for a good switch case, i dunno
+		//var menuArrange.length;
+				for (i in 0...menuArrange.length){
+
+					//if (menuArrange)
+
+					var testPoop:MenuIcon;
+					var xPos:Int;
+					var yPos:Int;
+
+					xPos = horizPositions[ Std.int(rows - menuArrange.indexOf(menuArrange[i]) ) ];
+					yPos = verticPositions[ Std.int(columns / menuArrange.indexOf(menuArrange[i]))       ];
+					testPoop = new MenuIcon(
+					//horizPositions[ Std.int( (menuArrange.indexOf(menuArrange[i], 0) / rows * 10))],
+					//verticPositions[  Std.int( menuArrange.indexOf(menuArrange[i], 0)  / columns * 10)],
+					xPos,
+					yPos,
+					menuArrange[i]);
+					//trace(menuArrange[i] + ", x: " + Std.int(menuArrange.indexOf(menuArrange[i], 0) / rows * 10) + ", y: " + Std.int(menuArrange.indexOf(menuArrange[i], 0) / columns * 10) + ", Index: " + menuArrange.indexOf(menuArrange[i], 0));
+					trace("Index: " + menuArrange.indexOf(menuArrange[i], 0) + ", X: " + xPos + ", Y: " + yPos);
+				//add(testPoop);
+					curMadeSelectionIcons.add(testPoop);
+
+					// seems like duplicates just spawn in the same place as the OG ones? idk for sure
 				}
-				testPoop = new MenuIcon(horizPositions[i], verticPositions[newYPos], menuArrange[i]);
-				trace(menuArrange[i]);
-				curMadeSelectionIcons.add(testPoop);
-			}
-		}
 
-		add (curMadeSelectionIcons);
+				add (curMadeSelectionIcons);
+
+
 	}
 
 	override function update(elapsed:Float){
@@ -99,5 +116,18 @@ var menuArrange:Array<String>;
 	private function closeSub():Void
 	{
 		close();
+	}
+
+	function makeBlankSpaces(){
+		for (h in 0...verticPositions.length){
+				for (i in 0...horizPositions.length){
+					iconSlot = new FlxSprite();
+					iconSlot.loadGraphic("assets/images/menuBody/mainMenu/Blank Icon.png");
+					iconSlot.x = horizPositions[i];
+					iconSlot.y = verticPositions[h];
+					iconSlot.updateHitbox();
+					add(iconSlot);
+				}
+			}
 	}
 }
